c47fa32aee0a1692264f656f5e7a585fdf65fa36
OrreroVV <1271966049@qq.com>
2024-07-07 14:43:13 +0800

    add demo

---
+++ tests/test_client.cpp
@@ -0,0 +1,49 @@
+// client.cpp
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 6666
+#define SERVER_IP "8.138.86.207"
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+
+    // 创建 socket 文件描述符
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "Socket creation error" << std::endl;
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // 将地址转换成二进制形式
+    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        return -1;
+    }
+
+    // 连接到服务器
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Connection Failed" << std::endl;
+        return -1;
+    }
+
+    // 发送消息到服务器
+    const char *message = "Hello from client";
+    send(sock, message, strlen(message), 0);
+    std::cout << "Message sent to server" << std::endl;
+
+    // 读取服务器的响应
+    int valread = read(sock, buffer, BUFFER_SIZE);
+    std::cout << "Received from server: " << buffer << std::endl;
+
+    // 关闭连接
+    close(sock);
+    return 0;
+}
---
+++ tests/test_server.cpp
@@ -0,0 +1,63 @@
+// server.cpp
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 6666
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+
+    // 创建 socket 文件描述符
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 绑定到端口 6666
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // 监听连接
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Server is listening on port " << PORT << std::endl;
+
+    // 接受客户端连接
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // 读取客户端发送的数据
+    int valread = read(new_socket, buffer, BUFFER_SIZE);
+    std::cout << "Received from client: " << buffer << std::endl;
+
+    // 发送响应到客户端
+    const char *response = "Hello from server";
+    send(new_socket, response, strlen(response), 0);
+    std::cout << "Response sent to client" << std::endl;
+
+    // 关闭连接
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
